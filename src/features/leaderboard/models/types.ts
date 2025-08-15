7df5eb6a40aaf03a8f30270c0e15c8654959396a
alvaromartinezreche98-svg <alvaro.martinez.reche98@gmail.com>
2025-08-15 18:49:55 +0200

    refactor: simplify navigation and remove leaderboard

--- src/features/leaderboard/models/types.ts
+++
@@ -1,9 +0,0 @@
-export interface TopWorkoutUser {
-  userId: string;
-  userName: string;
-  userImage: string | null;
-  totalWorkouts: number;
-  lastWorkoutAt: Date | null;
-  averageWorkoutsPerWeek: number;
-  memberSince: Date;
-}
